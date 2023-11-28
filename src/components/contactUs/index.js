4a9c03749044253672a92a801db7c0f31779a9c7
DevDennyBeyondLimits <amc.dev@beyondlimitsmm.com>
2023-11-28 16:09:27 +0630

    feat: Review changes

--- src/components/contactUs/index.js
+++ src/components/contactUs/index.js
@@ -1,3 +1,2 @@
 export { default as Hero } from "./Hero.astro";
-export { default as Contact } from "./Contact.jsx";
-export { default as ContactForm } from "./ContactForm.jsx";
+export { default as Contact } from "./Contact.astro";
