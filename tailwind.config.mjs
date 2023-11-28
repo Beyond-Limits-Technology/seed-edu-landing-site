81e47b3f97f3e4e764a5ae51f57bc9312f93dae8
Khant Sithu <142285082+beyond-khantsithu@users.noreply.github.com>
2023-11-28 17:00:08 +0630

    feat: about us (#4)
    * feat: about us
    
    * refactor: folder name
    
    ---------
    
    Co-authored-by: Khant Sithu <khantseithuu@gmail.com>

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,6 +6,10 @@ export default {
       fontFamily: {
         switzer: ["Switzer", "sans-serif"],
       },
+      backgroundImage: {
+        "about-hero":
+          "url('https://images.unsplash.com/photo-1624138784614-87fd1b6528f8?q=80&w=1333&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
+      },
     },
   },
   plugins: [],
