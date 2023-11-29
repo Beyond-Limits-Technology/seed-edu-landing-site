49578ce791773bb835fb419f1f9313d4f630e38c
zayminmaw <zayminmaw77@gmail.com>
2023-11-29 12:29:26 +0630

    hotfix(home): padding

--- src/components/common/Drawer.jsx
+++ src/components/common/Drawer.jsx
@@ -26,13 +26,13 @@ const Drawer = ({ showDrawer, setShowDrawer }) => {
               fill="currentColor"
               xmlns="http://www.w3.org/2000/svg"
             >
-              <g clip-path="url(#clip0_429_11083)">
+              <g clipPath="url(#clip0_429_11083)">
                 <path
                   d="M7 7.00006L17 17.0001M7 17.0001L17 7.00006"
                   stroke="currentColor"
-                  stroke-width="2.5"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeWidth="2.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                 />
               </g>
             </svg>
