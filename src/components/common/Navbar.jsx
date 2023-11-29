04f74328077a07fb0d9affb0b0dff06ee65becd7
zayminmaw <zayminmaw77@gmail.com>
2023-11-29 12:31:42 +0630

    hotfix: removed a bar top of navbar

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -12,17 +12,6 @@ const Navbar = () => {
 
   return (
     <>
-      <div className="w-full h-[40px] bg-[#3A1E13] md:block hidden">
-        <div className="max-w-7xl h-full flex justify-between items-center mx-auto lg:px-2 px-4">
-          <div className="flex items-center gap-4 text-[#F5BB00] ">
-            <a href="mailto:dennykate22@gmail.com">dennykate22@gmail.com</a>
-            <a href="tel:09969969969">09969969969</a>
-          </div>
-
-          <div className="flex items-center gap-4"></div>
-        </div>
-      </div>
-
       <div className="w-full h-[80px] shadow-md sticky top-0 left-0 z-[100] bg-white">
         <div className="max-w-7xl mx-auto h-full  flex justify-between items-center lg:px-2 px-4">
           <div>Logo</div>
