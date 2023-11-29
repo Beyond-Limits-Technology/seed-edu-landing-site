12aab47ff0cc28df5763dcc09e3d298c9fe056fa
Khant Sithu <khantseithuu@gmail.com>
2023-11-29 10:37:56 +0630

    hotfix: color theming

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,12 +3,43 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      colors: {
+        primary: {
+          100: "#f4e8cd",
+          200: "#ead09b",
+          300: "#dfb968",
+          400: "#d5a136",
+          500: "#ca8a04",
+          600: "#a26e03",
+          700: "#795302",
+          800: "#513702",
+          900: "#281c01",
+        },
+        dark: {
+          100: "#cccccc",
+          200: "#999999",
+          300: "#666666",
+          400: "#333333",
+          500: "#000000",
+          600: "#000000",
+          700: "#000000",
+          800: "#000000",
+          900: "#000000",
+        },
+        secondary: {
+          100: "#ffffff",
+          200: "#ffffff",
+          300: "#ffffff",
+          400: "#ffffff",
+          500: "#ffffff",
+          600: "#cccccc",
+          700: "#999999",
+          800: "#666666",
+          900: "#333333",
+        },
+      },
       fontFamily: {
         switzer: ["Switzer", "sans-serif"],
-      },
-      backgroundImage: {
-        "about-hero":
-          "url('https://images.unsplash.com/photo-1624138784614-87fd1b6528f8?q=80&w=1333&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
       },
     },
   },
