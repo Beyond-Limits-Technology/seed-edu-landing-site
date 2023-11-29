53822261d6ac71d74cc16ad948e81d8908937bd2
Zay Min Maw <59911091+zayminmaw@users.noreply.github.com>
2023-11-29 12:18:02 +0630

    feat: added why seed section (#6)

---
+++ src/data/whySeedData.js
@@ -0,0 +1,47 @@
+export default [
+  {
+    title: "Expert Guidance",
+    info: [
+      {
+        title: "Personalized Expertise",
+        subtext:
+          "Our seasoned consultants offer personalized guidance, tailoring each student's journey for success with a customized approach to meet their unique goals.",
+      },
+      {
+        title: "Up-to-Date Insights",
+        subtext:
+          "We keep you ahead with current insights for a competitive educational edge.",
+      },
+    ],
+  },
+  {
+    title: "Comprehensive Support",
+    info: [
+      {
+        title: "End-to-End Assistance",
+        subtext:
+          "Navigate academics stress-free with our comprehensive support from program selection to admission.",
+      },
+      {
+        title: "Career Counseling and Planning",
+        subtext:
+          "We go beyond academics, offering career counseling and strategic planning to align education with long-term goals, empowering informed decisions for the future.",
+      },
+    ],
+  },
+  {
+    title: "Access to Opportunities",
+    info: [
+      {
+        title: "Global Network",
+        subtext:
+          "Unlock global opportunities with our vast network, connecting students to prestigious universities, specialized programs, and unique scholarships.",
+      },
+      {
+        title: "Opportunities Tailored to Ambitions",
+        subtext:
+          "We align ambitions with opportunities, ensuring an enriching educational journey that leads to future success in chosen fields.",
+      },
+    ],
+  },
+];
