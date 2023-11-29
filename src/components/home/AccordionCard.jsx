bcd0c53e7fcccf6aabf0ed130ea3b34335063822
zayminmaw <zayminmaw77@gmail.com>
2023-11-29 16:18:36 +0630

    feat: added faq preview

---
+++ src/components/home/AccordionCard.jsx
@@ -0,0 +1,40 @@
+const AccordionCard = ({ title, answer, index, value, callback }) => {
+  return (
+    <>
+      <h2>
+        <button
+          onClick={() => callback(index)}
+          type="button"
+          className="flex items-center justify-between w-full py-5 font-medium rtl:text-right border-b border-gray-200 gap-3"
+        >
+          <span>{title}</span>
+          <svg
+            data-accordion-icon
+            className={`w-3 h-3 ${!(index === value) && "rotate-180"} shrink-0`}
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 10 6"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M9 5 5 1 1 5"
+            ></path>
+          </svg>
+        </button>
+      </h2>
+      {index === value && (
+        <div>
+          <div className="py-5">
+            <p className="mb-2 text-gray-600">{answer}</p>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default AccordionCard;
---
+++ src/components/home/CompoundAccordionCards.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import AccordionCard from "./AccordionCard";
+
+const CompoundAccordionCard = () => {
+  const [show, setShow] = useState(1);
+  return (
+    <>
+      <AccordionCard
+        title="hello"
+        answer="hello ans"
+        index={1}
+        value={show}
+        callback={(index) =>
+          setShow((prev) => (prev === index ? undefined : index))
+        }
+      />
+      <AccordionCard
+        title="hello"
+        answer="hello ans"
+        index={2}
+        value={show}
+        callback={(index) =>
+          setShow((prev) => (prev === index ? undefined : index))
+        }
+      />
+      <AccordionCard
+        title="hello"
+        answer="hello ans"
+        index={3}
+        value={show}
+        callback={(index) =>
+          setShow((prev) => (prev === index ? undefined : index))
+        }
+      />
+    </>
+  );
+};
+
+export default CompoundAccordionCard;
