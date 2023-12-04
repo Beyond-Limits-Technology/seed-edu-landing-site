8907c23090d359e98d1d93f8bc72e30ce9f2ebac
DevDennyBeyondLimits <amc.dev@beyondlimitsmm.com>
2023-12-04 16:58:00 +0630

    feat: Minor Navbar UI Fix

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,24 +1,42 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 
 import navbarData from "../../data/navbarData";
 import NavbarItem from "./NavbarItem";
 import Drawer from "./Drawer";
+import useScrollOffset from "../../hooks/useScrollOffset";
 // import useDisableScroll from "../hooks/useDisableScroll";
 
-const Navbar = () => {
+const alwayActivePaths = ["/privacy-policy"];
+
+const Navbar = ({ pathname }) => {
   const [showDrawer, setShowDrawer] = useState(false);
+  const offset = useScrollOffset();
+
+  const isScrollActive = useMemo(() => {
+    if (alwayActivePaths.find((path) => path === pathname)) return true;
+
+    return offset > 0;
+  }, [offset]);
 
   // useDisableScroll(setShowDrawer);
 
   return (
     <>
-      <div className="w-full h-[80px] shadow-md sticky top-0 left-0 z-[100] bg-white">
+      <div
+        className={`w-full h-[80px] fixed top-0 left-0 z-[100] ${
+          isScrollActive ? "bg-white shadow-md " : "bg-transparent"
+        }`}
+      >
         <div className="max-w-7xl mx-auto h-full  flex justify-between items-center lg:px-2 px-4">
           <div>Logo</div>
 
           <div className="h-full lg:flex hidden items-center xl:gap-4 gap-4">
             {navbarData.map((data) => (
-              <NavbarItem key={data.name} data={data} />
+              <NavbarItem
+                isScrollActive={isScrollActive}
+                key={data.name}
+                data={data}
+              />
             ))}
 
             <div className="w-auto h-auto bg-black">
--- src/components/common/NavbarItem.jsx
+++ src/components/common/NavbarItem.jsx
@@ -1,21 +1,30 @@
-const NavbarItem = ({ data }) => {
+const NavbarItem = ({ data, isScrollActive }) => {
   return (
     <div className="h-full flex justify-center items-center">
       <div className="flex items-center gap-2 py-2 group">
         <div className="flex items-center gap-2">
           <a
             href={data.link}
-            className="text-[14px] font-[400] uppercase cursor-pointer tracking-wide"
+            className={`text-[14px] font-[400] uppercase cursor-pointer tracking-wide ${
+              isScrollActive ? "text-black" : "text-white"
+            }`}
           >
             {data.name}
           </a>
 
           {data.subData.length > 0 && (
-            <img
-              src="/assets/icons/down.png"
-              alt=""
-              className="w-[10px] cursor-pointer"
-            />
+            <svg
+              fill={isScrollActive ? "black" : "white"}
+              xmlns="http://www.w3.org/2000/svg"
+              width="800px"
+              height="800px"
+              viewBox="0 0 52 52"
+              enableBackground="new 0 0 52 52"
+              xmlSpace="preserve"
+              className="w-[14px]"
+            >
+              <path d="M8.3,14h35.4c1,0,1.7,1.3,0.9,2.2L27.3,37.4c-0.6,0.8-1.9,0.8-2.5,0L7.3,16.2C6.6,15.3,7.2,14,8.3,14z" />
+            </svg>
           )}
         </div>
 
@@ -25,7 +34,7 @@ const NavbarItem = ({ data }) => {
               <div
                 className={`bg-white rounded-md shadow-2xl  flex-col items-start 
            transition-all duration-300 ease-in-out justify-center 
-           min-w-[240px] min-h-[100px] py-4 hidden group-hover:flex`}
+           min-w-[240px]  py-4 hidden group-hover:flex`}
               >
                 {data.subData?.map((data) => (
                   <div key={data.name} className="relative group/nested w-full">
