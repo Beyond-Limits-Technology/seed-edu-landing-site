7ff3a9b012331cd0632c5bde408f3f96d161b198
DevDennyBeyondLimits <amc.dev@beyondlimitsmm.com>
2024-01-09 10:47:20 +0630

    feat: scholarship ui fix

---
+++ src/data/scholarshipContentData.js
@@ -0,0 +1,17 @@
+export default [
+  {
+    content: "Save on your course fees with the assistance of SEED Education",
+  },
+  {
+    content:
+      "Australian universities and colleges offer diverse scholarships to attract top global talent. These programs, funded by both the government and institutions, provide significant financial assistance for education and living expenses, making studying in Australia more accessible for high-achieving students.",
+  },
+  {
+    content:
+      "SEED Education, in close collaboration with Australian schools and colleges, provides you with exclusive access to the most recent scholarships and bursaries available for international students. We ensure that you stay informed about the latest opportunities and guide you through the application process.",
+  },
+  {
+    content:
+      "For more details of the scholarship consultation, book with our expert by filling out below fields and drop a message.",
+  },
+];
