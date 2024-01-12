63548137f59373a4d6867a0e771f467253d056dc
zayminmaw <zayminmaw77@gmail.com>
2024-01-12 16:22:42 +0630

    hotfix: font size

--- src/components/faqs/Content.jsx
+++ src/components/faqs/Content.jsx
@@ -5,8 +5,8 @@ const Content = () => {
   const [show, setShow] = useState(undefined);
 
   return (
-    <div className="max-w-7xl mx-auto py-[100px] px-5">
-      <h6 className="sm:text-5xl text-4xl font-bold text-black text-center">
+    <div className="max-w-7xl mx-auto pt-16 pb-20 px-5">
+      <h6 className=" text-3xl font-bold text-black text-center">
         Frequently Asked Questions
       </h6>
 
