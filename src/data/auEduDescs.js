fc6443104dc6b4072b9056867b9569b34c855906
DevDennyBeyondLimits <142196976+DevDennyBeyondLimits@users.noreply.github.com>
2024-01-16 15:26:02 +0630

    feat: minor fix (#62)

---
+++ src/data/auEduDescs.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    label:
+      " Australia has many study options for international students, with over 1,100 institutions and 22,000 courses. The education system includes primary, secondary, and tertiary levels, guided by the Australian Qualifications Framework (AQF), which shows how qualifications connect at each education level.",
+  },
+  {
+    label:
+      "International students can study at all levels, from primary school to higher education. Tertiary education in Australia includes universities and vocational education and training (VET) or TAFE colleges.",
+  },
+  {
+    label:
+      "For quality assurance, there are organizations like TEQSA for higher education and ASQA for VET institutions. They handle registration and accreditation matters.",
+  },
+  {
+    label: "Australia’s Laws ensure quality education and protection for international students, including the ESOS Act 2000 and the National Code of Practice for Overseas Students 2007. These set consistent standards for education providers.",
+  },
+  {
+    label: "If you are ready to learn more about studying in Australia, contact us today to talk to an education expert!",
+  },
+];
