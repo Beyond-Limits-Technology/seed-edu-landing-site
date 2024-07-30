13a5c3a651311a188cf4cd19501385c441763102
Hein <heinminthant1999@gmail.com>
2024-07-30 12:52:07 +0700

    feat: add testimonial item

---
+++ src/components/testimonials/testimonials.js
@@ -0,0 +1,17 @@
+export const testimonials = [
+  {
+    name: "Panuwat",
+    major: "Advanced Diploma of Civil Construction Design",
+    university: "Danford College Melbourne",
+    image: "panuwat.png",
+    testimonial: `SEED Education has made a significant impact on my educational journey, and I am grateful for their unwavering support. Their commitment to helping students like me achieve our goals is truly remarkable. Thank you, SEED Education, for making my dreams of studying in Australia a reality.`,
+  },
+
+  {
+    name: "Tokio",
+    major: "Advanced Diploma of Program Management",
+    university: "Lonsdale Institute Melbourne",
+    image: "tokio.png",
+    testimonial: `SEED Education's unwavering support and dedication have been invaluable. They never once complained about my numerous inquiries and always made me feel supported and informed. Their commitment to their clients' success is truly commendable. Thank you, SEED Education, for making my journey to further my education abroad a seamless and positive experience.`,
+  },
+];
